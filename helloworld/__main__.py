e5db448797667396fad5378dedefb153754d3df6
Pablo Nguyen <81577310+pnguy150@users.noreply.github.com>
2025-12-03 10:09:10 -0800

    Add files via upload

---
+++ helloworld/__main__.py
@@ -0,0 +1,111 @@
+import uvicorn
+
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import (
+    AgentCapabilities,
+    AgentCard,
+    AgentSkill,
+)
+from starlette.applications import Starlette
+
+from helloworld_agent_executor import (
+    HelloWorldAgentExecutor,  # type: ignore[import-untyped]
+)
+from sqloptimizer_agent_executor import (
+    SQLOptimizerAgentExecutor, # type: ignore[import-untyped]
+)
+from starlette.routing import Mount
+
+if __name__ == '__main__':
+    # --8<-- [start:AgentSkill]
+    skill = AgentSkill(
+        id='hello_world',
+        name='Returns hello world',
+        description='just returns hello world',
+        tags=['hello world'],
+        examples=['hi', 'hello world'],
+    )
+    # --8<-- [end:AgentSkill]
+
+    sql_optimizer_skill = AgentSkill(
+        id="sql_optimizer",
+        name="SQL Optimizer",
+        description="optimizes sql statements",
+        tags=["sql", "optimize"],
+        examples=["Optimize this sql statement"]
+    )
+
+    extended_skill = AgentSkill(
+        id='super_hello_world',
+        name='Returns a SUPER Hello World',
+        description='A more enthusiastic greeting, only for authenticated users.',
+        tags=['hello world', 'super', 'extended'],
+        examples=['super hi', 'give me a super hello'],
+    )
+
+    # --8<-- [start:AgentCard]
+    # This will be the public-facing agent card
+    public_agent_card = AgentCard(
+        name='Hello World Agent',
+        description='Just a hello world agent',
+        url='http://localhost:9999/',
+        version='1.0.0',
+        default_input_modes=['text'],
+        default_output_modes=['text'],
+        capabilities=AgentCapabilities(streaming=True),
+        skills=[skill],  # Only the basic skill for the public card
+        supports_authenticated_extended_card=True,
+    )
+    # --8<-- [end:AgentCard]
+
+    # This will be the authenticated extended agent card
+    # It includes the additional 'extended_skill'
+    specific_extended_agent_card = public_agent_card.model_copy(
+        update={
+            'name': 'Hello World Agent - Extended Edition',  # Different name for clarity
+            'description': 'The full-featured hello world agent for authenticated users.',
+            'version': '1.0.1',  # Could even be a different version
+            # Capabilities and other fields like url, default_input_modes, default_output_modes,
+            # supports_authenticated_extended_card are inherited from public_agent_card unless specified here.
+            'skills': [
+                skill,
+                extended_skill,
+            ],  # Both skills for the extended card
+        }
+    )
+    sql_optimizer_agent_card = AgentCard(
+        name='SQL Optimizere Agent',
+        description='Optimizes SQL statements',
+        url='http://localhost:9999/',
+        version='1.0.0',
+        default_input_modes=['text'],
+        default_output_modes=['text'],
+        capabilities=AgentCapabilities(streaming=True),
+        skills=[sql_optimizer_skill],  # Only the basic skill for the public card
+        supports_authenticated_extended_card=True,
+    )
+
+    request_handler = DefaultRequestHandler(
+        agent_executor=HelloWorldAgentExecutor(),
+        task_store=InMemoryTaskStore(),
+    )
+    request_handler_sql = DefaultRequestHandler(
+        agent_executor=SQLOptimizerAgentExecutor(),
+        task_store=InMemoryTaskStore(),
+    )
+
+    server_hello = A2AStarletteApplication(
+        agent_card=public_agent_card,
+        http_handler=request_handler,
+        extended_agent_card=specific_extended_agent_card,
+    )
+    server_sql = A2AStarletteApplication(
+        agent_card=sql_optimizer_agent_card,
+        http_handler=request_handler_sql
+    )
+    app = Starlette(routes=[Mount("/helloworld", app=server_hello.build()),
+                            Mount("/SQLOptimizer", app=server_sql.build())])
+
+    uvicorn.run(app, host='0.0.0.0', port=9999)
